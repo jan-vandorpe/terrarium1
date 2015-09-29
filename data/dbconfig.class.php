c5f94e901252e8b83a7fbf36d52b1d3173340e67
Ely <scoofy@gmail.com>
2015-09-29 11:41:45 +0200

    Nieuwe DB login die werkt nu

--- data/dbconfig.class.php
+++ data/dbconfig.class.php
@@ -3,6 +3,6 @@
 class DBConfig
   {
     public static $DB_CONNSTRING = "mysql:host=localhost;dbname=terrarium";
-    public static $DB_USERNAME = "ID143224_terrari";
-	public static $DB_PASSWORD = "terraripwd";
+    public static $DB_USERNAME = "terra";
+	public static $DB_PASSWORD = "terra123";
   }
