726c90ead8f1d65ff6b57c5ac31800fcfb55a5d1
Ely <scoofy@gmail.com>
2015-09-29 10:42:01 +0200

    dbconfig aangepast met localhost connectie

--- data/dbconfig.class.php
+++ data/dbconfig.class.php
@@ -2,7 +2,7 @@
 
 class DBConfig
   {
-  public static $DB_CONNSTRING = "mysql051.hosting.combell.com:3306;dbname=ID143224_terrari";
-  public static $DB_USERNAME = "ID143224_terrari";
-  public static $DB_PASSWORD = "T3r1um_t";
+    public static $DB_CONNSTRING = "mysql:host=localhost;dbname=terrarium";
+    public static $DB_USERNAME = "cursusgebruiker";
+	public static $DB_PASSWORD = "cursuspwd";
   }
