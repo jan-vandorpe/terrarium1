6ca2e6d8bdf13b814fc4c6ae0d430775b9e11f66
Ely <scoofy@gmail.com>
2015-10-06 09:46:15 +0200

    Raster met div

--- presentation/raster.php
+++ presentation/raster.php
@@ -6,16 +6,19 @@ foreach($games as $game) {
         $grootte = $game->grootte;
     }
 }
-
+print ("<div class='matrix clearFix'>");
 for ($rij=1; $rij<=$grootte; $rij++) {
     for ($kol=1; $kol<=$grootte; $kol++) {
         $positie = organismeservice::checkPosition($kol,$rij,$_GET["game"]);
         if (isset($positie)) {
-            print (" [ ".$kol."/".$rij." ] ");
+//            print (" [ ".$kol."/".$rij." ] ");
+              print ("<div class='cell'><div class='innercell'><img src='img/carnivoor.svg' title='carnivoor'></div></div>");
         } else {
-            print (" ( ".$kol."/".$rij." )");
+//           print (" ( ".$kol."/".$rij." )");
+              print ("<div class='cell'><div class='innercell'>leeg</div></div>");
         }
     }
     print ("<br>");
 }
+print ("</div>");
 ?>
